df05ea88ff12c9ee11966caa3d7c58d1db3cfb1c
kdavis012218 <35358468+kdavis012218@users.noreply.github.com>
2018-02-24 22:51:53 -0500

    exercise 11

---
+++ ConsoleApp68/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp68
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int a = 1; a < 10; a = a + 1)
+            {
+                Console.WriteLine("value of a: {0}", a);
+                {
+                    
+                    Console.ReadKey();
+                    Console.WriteLine("Would you like to continue? y or n");
+                    Console.ReadLine();
+                    
+                    if input = "y";
+                    {
+                        return;
+                    }
+
+                    else
+                    Console.ReadKey();
+                }
+            }
+        }
+    }
+}
+
